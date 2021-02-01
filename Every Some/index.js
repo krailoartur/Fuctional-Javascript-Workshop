4298e918db5dc7242be2aadd6f9dfe846d0e9aba
krailoartur <krailo.artur1@gmail.com>
2021-02-01 17:05:33 +0200

    Every Some Complited

---
+++ Every Some/index.js
@@ -0,0 +1,10 @@
+function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every((submittedUser) => {
+      return goodUsers.some((goodUser) => {
+        return goodUser.id === submittedUser.id;
+      });
+    });
+  };
+}
+module.exports = checkUsersValid;
